c878206170b8381d5d4456ef508dc5aeb7dfd290
Emmanuel Lemaistre <manu9576@hotmail.fr>
2019-07-15 22:46:36 +0200

    Adding WebSite Resources

---
+++ wpf/Chap3 CustomControl/CircularMinuteTimer.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace wpf.Chap3_CustomControl
+{
+    /// <summary>
+    /// Interaction logic for CircularMinuteTimer.xaml
+    /// </summary>
+    public partial class CircularMinuteTimer : UserControl
+    {
+        public CircularMinuteTimer()
+        {
+            InitializeComponent();
+        }
+    }
+}
